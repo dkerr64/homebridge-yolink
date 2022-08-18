b19740c5a92450b8a5826017573a726227f6d820
David Kerr <david@kerrfamily.org>
2022-08-18 08:00:46 -0400

    Treat outlet as outlet... for issue #23

--- src/deviceHandlers.ts
+++ src/deviceHandlers.ts
@@ -12,6 +12,7 @@ import { initValveDevice, mqttValveDevice } from './valveDevice';
 import { initThermoHydroDevice, mqttThermoHydroDevice } from './thermoHydroDevice';
 import { initContactSensor, mqttContactSensor } from './contactDevice';
 import { initSwitchDevice, mqttSwitchDevice } from './switchDevice';
+import { initOutletDevice, mqttOutletDevice } from './outletDevice';
 import { initStatelessSwitch, mqttStatelessSwitch } from './statelessSwitch';
 
 export const deviceFeatures = {
@@ -22,7 +23,7 @@ export const deviceFeatures = {
   THSensor: { experimental: false, hasBattery: true },
   DoorSensor: { experimental: false, hasBattery: true },
   Siren: { experimental: true, hasBattery: true },
-  Switch: { experimental: true, hasBattery: true },
+  Switch: { experimental: true, hasBattery: false },
   Outlet: { experimental: true, hasBattery: false },
   SmartRemoter: { experimental: false, hasBattery: true },
 };
@@ -36,7 +37,7 @@ export const initDeviceService = {
   DoorSensor(this: YoLinkPlatformAccessory) { initContactSensor.bind(this)(); },
   Siren(this: YoLinkPlatformAccessory) { initSwitchDevice.bind(this)('alert', {'alarm':true}, {'alarm':false}); },
   Switch(this: YoLinkPlatformAccessory) { initSwitchDevice.bind(this)('closed', 'close', 'open'); },
-  Outlet(this: YoLinkPlatformAccessory) { initSwitchDevice.bind(this)('closed', 'close', 'open'); },
+  Outlet(this: YoLinkPlatformAccessory) { initOutletDevice.bind(this)('closed', 'close', 'open'); },
   SmartRemoter(this: YoLinkPlatformAccessory) { initStatelessSwitch.bind(this)(4); },
 };
 
@@ -49,6 +50,6 @@ export const mqttHandler = {
   DoorSensor(this: YoLinkPlatformAccessory, data) { mqttContactSensor.bind(this)(data); },
   Siren(this: YoLinkPlatformAccessory, data) { mqttSwitchDevice.bind(this)(data); },
   Switch(this: YoLinkPlatformAccessory, data) { mqttSwitchDevice.bind(this)(data); },
-  Outlet(this: YoLinkPlatformAccessory, data) { mqttSwitchDevice.bind(this)(data); },
+  Outlet(this: YoLinkPlatformAccessory, data) { mqttOutletDevice.bind(this)(data); },
   SmartRemoter(this: YoLinkPlatformAccessory, data) { mqttStatelessSwitch.bind(this)(data); },
 };
