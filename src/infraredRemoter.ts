0b5eb67d63027be28fbe8ac7f820a62007f19817
David Kerr <david@kerrfamily.org>
2023-11-19 14:04:22 -0500

    First pass at supporting IR blaster

--- src/infraredRemoter.ts
+++ src/infraredRemoter.ts
@@ -7,7 +7,7 @@
  *
  */
 
-import { CharacteristicValue } from 'homebridge';
+import { PlatformAccessory, CharacteristicValue } from 'homebridge';
 import { YoLinkHomebridgePlatform, YoLinkDevice } from './platform';
 import { YoLinkPlatformAccessory } from './platformAccessory';
 
@@ -17,39 +17,123 @@ import { YoLinkPlatformAccessory } from './platformAccessory';
  */
 export async function initInfraredRemoter(this: YoLinkPlatformAccessory): Promise<void> {
   const platform: YoLinkHomebridgePlatform = this.platform;
+  const accessory: PlatformAccessory = this.accessory;
   const device: YoLinkDevice = this.accessory.context.device;
 
-  platform.log.warn(`YoLink device type: '${device.type}' is under development (${device.deviceMsgName}) (initialize)`
-    + platform.reportError + JSON.stringify(device, null, 2));
+  this.setMethod = 'send';
+  this.irKey = [];
 
+  platform.log.warn('Initialize infrared remoter, find number of learned buttons...');
+  await handleGet.bind(this)();
+  const nLearned = device.data.keys.reduce((n: number, x: boolean) => (x === true) ? n++ : n, 0);
+  platform.log.warn(`Initialize infrared remoter with ${nLearned} button${(nLearned > 1) ? 's' : ''}`);
+
+  // serviceLabel required when multiple services of same type on one accessory
+  this.serviceLabel = accessory.getService(platform.Service.ServiceLabel) || accessory.addService(platform.Service.ServiceLabel);
+  this.serviceLabel.setCharacteristic(platform.Characteristic.Name, device.name);
+  this.serviceLabel.getCharacteristic(platform.Characteristic.ServiceLabelNamespace).onGet(() => {
+    return (this.platform.Characteristic.ServiceLabelNamespace.ARABIC_NUMERALS);
+  });
+
+  let slIndex = 1;
+  device.data.keys.forEach((b: boolean, i: number) => {
+    if (b) {
+      platform.log.warn(`Add switch for learned button index: ${i}`);
+      this.irKey[i] = {};
+      this.irKey[i].switchService = accessory.getService(`IR Key ${i}`)
+        || accessory.addService(platform.Service.Switch, `IR Key ${i}`, `irkey${i}`);
+      this.irKey[i].switchService.setCharacteristic(platform.Characteristic.Name, `${device.name} IR Key ${i}`);
+      this.irKey[i].switchService.setCharacteristic(platform.Characteristic.ServiceLabelIndex, slIndex++);
+      this.irKey[i].switchService.getCharacteristic(platform.Characteristic.On)
+        .onGet(handleGet.bind(this, i))
+        .onSet(handleSet.bind(this, i));
+    }
+  });
+
+  // timer to regularly update the data... really only to monitor battery level.
   this.refreshDataTimer(handleGet.bind(this));
 }
 
 /***********************************************************************
  * handleGet
  *
+ * Example of message received:
+ * Note that array of keys is length 64
+ * {
+ *   "battery": 4,
+ *   "keys": [
+ *     true,
+ *     false,
+ *     .
+ *     .
+ *     .
+ *     false,
+ *     false
+ *   ],
+ *   "version": "030a",
+ *   "time": "2023-10-18T16:20:54.000Z",
+ *   "tz": 0,
+ *   "loraInfo": {
+ *     "netId": "010203",
+ *     "signal": -23,
+ *     "gatewayId": "abcdef1234567890",
+ *     "gateways": 1
+ *   }
+ * }
  */
-async function handleGet(this: YoLinkPlatformAccessory): Promise<CharacteristicValue> {
+async function handleGet(this: YoLinkPlatformAccessory, keyNumber = -1): Promise<CharacteristicValue> {
   const platform: YoLinkHomebridgePlatform = this.platform;
   const device: YoLinkDevice = this.accessory.context.device;
   // serialize access to device data.
   const releaseSemaphore = await device.semaphore.acquire();
   try {
     if (await this.checkDeviceState(platform, device)) {
-
-      platform.log.warn(`YoLink device type: '${device.type}' is under development (${device.deviceMsgName}) (handleGet)`
-        + platform.reportError + JSON.stringify(device.data, null, 2));
-
+      const batteryMsg = (device.hasBattery) ? `, Battery: ${device.data.battery}` : '';
+      this.logDeviceState(device, `Key Number: ${keyNumber}${batteryMsg}`);
     } else {
       platform.log.error(`Device offline or other error for ${device.deviceMsgName}`);
     }
   } catch (e) {
     const msg = (e instanceof Error) ? e.stack : e;
-    platform.log.error('Error in InfraredRemoter handleGet' + platform.reportError + msg);
+    platform.log.error('Error in infrared remoter handleGet' + platform.reportError + msg);
   } finally {
     releaseSemaphore();
   }
+  // IR Remoter buttons are always "off" as they are stateless
   return (false);
+}
+
+/***********************************************************************
+ * handleSet
+ *
+ * This is an example of JSON object returned.
+ *
+ * { TBD }
+ *
+ */
+async function handleSet(this: YoLinkPlatformAccessory, keyNumber = -1, value: CharacteristicValue): Promise<void> {
+  const platform: YoLinkHomebridgePlatform = this.platform;
+  const device: YoLinkDevice = this.accessory.context.device;
+  // serialize access to device data.
+  const releaseSemaphore = await device.semaphore.acquire();
+  try {
+    if (keyNumber >= 0 && value === true) {
+      const data = (await platform.yolinkAPI.setDeviceState(platform, device, { 'key': keyNumber }, this.setMethod))?.data;
+      // error will have been thrown in yolinkAPI if data not valid
+      this.log.warn(`Send infrared key number ${keyNumber} returned:\n${JSON.stringify(data, null, 2)}`);
+      // Sending IR signal is stateless, make sure that the switch is turned off after slight delay
+      setTimeout(() => {
+        this.irKey[keyNumber].switchService.updateCharacteristic(platform.Characteristic.On, false);
+      }, 50);
+    } else {
+      this.log.warn(`Cannot turn off infrared Key number ${keyNumber}, switch is stateless and always off`);
+    }
+  } catch (e) {
+    const msg = (e instanceof Error) ? e.stack : e;
+    platform.log.error('Error in SwitchDevice handleGet' + platform.reportError + msg);
+  } finally {
+    releaseSemaphore();
+  }
 }
 
 /***********************************************************************
@@ -62,9 +146,15 @@ export async function mqttInfraredRemoter(this: YoLinkPlatformAccessory, message
   // serialize access to device data.
   const releaseSemaphore = await device.semaphore.acquire();
   try {
+    device.updateTime = Math.floor(new Date().getTime() / 1000) + device.config.refreshAfter;
+    const mqttMessage = `MQTT: ${message.event} for device ${device.deviceMsgName}`;
+    const event = message.event.split('.');
+    // const batteryMsg = (device.hasBattery) ? `, Battery: ${message.data.battery}`: '';
 
-    platform.log.warn(`YoLink device type: '${device.type}' is under development (${device.deviceMsgName}) (MQTT)`
-      + platform.reportError + JSON.stringify(message, null, 2));
+    switch (event[1]) {
+      default:
+        platform.log.warn(mqttMessage + ' not supported.' + platform.reportError + JSON.stringify(message, null, 2));
+    }
 
   } catch (e) {
     const msg = (e instanceof Error) ? e.stack : e;
